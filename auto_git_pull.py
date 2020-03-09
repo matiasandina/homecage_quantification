b6a79319820980d6f9cca95f882965fc4a30feb0
matiasandina <matiasandina@gmail.com>
2020-03-09 10:17:52 -0400

    modifying cron jobs so that they don't create duplicates

--- auto_git_pull.py
+++ auto_git_pull.py
@@ -4,10 +4,31 @@ from crontab import CronTab
 # this is what we want to run
 cmd_command = "cd ~/homecage_quantification/ && git pull"
 
+job_comment = 'automatic git pull'
+
+# get cron object
 cron = CronTab(user="pi")
-# create a new job
-job = cron.new(command = cmd_command, comment='automatic git pull')
-# schedule it 
-job.hour.every(2)
-# write the program
-cron.write()
+# don't create a new job if it is already there
+# first check if it's already there
+
+job_exists = any(list(map(lambda x: x.comment == job_comment, cron)))
+
+if job_exists:
+    for job in cron:
+        if job.comment == job_comment:
+            print("job named: " + job_comment + " already exists, scheduling 2 hours")
+            job.hour.every(2)
+            # write the program
+            cron.write()
+else:
+    # only here, create it
+    # create a new job
+    job = cron.new(command = cmd_command, comment=job_comment)
+    # schedule it every minute
+    job.hour.every(2)
+    # write the program
+    cron.write()
+
+print("Current cron tab (same as $ crontab -l)")
+print("--------------------------------------")
+print(cron)
--- setup_cron.py
+++ setup_cron.py
@@ -2,10 +2,31 @@ from crontab import CronTab
 # this is what we want to run
 cmd_command = "bash ~/homecage_quantification/send_ip.sh"
 
+job_comment = "send ip to choilab1"
+
+# Get cron object
 cron = CronTab(user="pi")
-# create a new job
-job = cron.new(command = cmd_command, comment='send ip to choilab1')
-# schedule it every minute
-job.minute.every(1)
-# write the program
-cron.write()
+# don't create a new job if it is already there
+# first check if it's already there
+
+job_exists = any(list(map(lambda x: x.comment == job_comment, cron)))
+
+if job_exists:
+    for job in cron:
+        if job.comment == job_comment:
+            print("job named: " + job_comment + " already exists, scheduling 1 minute")
+            job.minute.every(1)
+            # write the program
+            cron.write()
+else:
+    # only here, create it
+    # create a new job
+    job = cron.new(command = cmd_command, comment=job_comment)
+    # schedule it every minute
+    job.minute.every(1)
+    # write the program
+    cron.write()
+
+print("Current cron tab (same as $ crontab -l)")
+print("--------------------------------------")
+print(cron)
