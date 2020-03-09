a50feba5c27aa210579daeecf8d20f2c7f9fa5c4
Matias Andina <matiasandina@gmail.com>
2020-03-09 10:24:35 -0400

    adding message to camera

--- preview_camera.py
+++ preview_camera.py
@@ -15,8 +15,8 @@ while(True):
 	
 	cv2.imshow("preview", cam.read())
 	k = cv2.waitKey(1)
-	cv2.putText(frame, "Preview:"),(10, 50),cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 1)
-	cv2.putText(frame, "press 'q' to quit"),(10, 100),cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 1)
+	cv2.putText(frame, "Preview:",(10, 50),cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 1)
+	cv2.putText(frame, "press 'q' to quit",(10, 100),cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 1)
 	if k == ord("q"):
 		break
 
