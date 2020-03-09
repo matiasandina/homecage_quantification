5a9060cb251d4821b15dbba7f3838b0b9531ac04
Matias Andina <matiasandina@gmail.com>
2020-03-09 10:26:09 -0400

    reorder because has to put text before showing

--- preview_camera.py
+++ preview_camera.py
@@ -13,10 +13,11 @@ cam = VideoCamera(
 
 while(True):
 	
-	cv2.imshow("preview", cam.read())
-	k = cv2.waitKey(1)
+	frame = cam.read()
 	cv2.putText(frame, "Preview:",(10, 50),cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 1)
 	cv2.putText(frame, "press 'q' to quit",(10, 100),cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 1)
+	cv2.imshow("preview", frame)
+	k = cv2.waitKey(1)
 	if k == ord("q"):
 		break
 
