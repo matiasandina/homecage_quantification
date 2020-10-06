633b3fb5ca8c550d57e4e4700e6493a07e628c33
Matias Andina <matiasandina@gmail.com>
2020-10-06 08:59:03 -0400

    fixing stuff on cpu_temp.sh

--- cpu_temp.sh
+++ cpu_temp.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-prtintf "%-15s%5s\n" "TIMESTAMP" "Temp"
+printf "%-15s%5s\n" "TIMESTAMP" "Temp"
 printf "%20s\n" "----------------------"
 
 while true
 do
-		temp=$(vcgencmd measure_temp)
+		# get temp and only the numeric values
+		temp=$(vcgencmd measure_temp | egrep -o '[0-9]*\.[0-9]*')
 		timestamp=$(date)
-		printf "%-15s%5s\n" "$timestamp" "$temp"
+		printf "%-15s%5s\n" "$timestamp" " " "$temp"
 		sleep 10
 done
