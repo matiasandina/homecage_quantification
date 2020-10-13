3fe4a785c95f3b5a09985555daf4146eb7b84fd0
Matias Andina <matiasandina@gmail.com>
2020-10-13 12:53:07 -0400

    add ram monitor

--- QC/monitor_rpi.py
+++ QC/monitor_rpi.py
@@ -28,15 +28,18 @@ while True:
 	cpu_temp = psutil.sensors_temperatures()["cpu-thermal"][0].current
 	# we could also use bash
 	#cpu_temp = os.popen("vcgencmd measure_temp | egrep -o '[0-9]*\.[0-9]*'").read().replace("\n", "")
+	# ram
+	ram = psutil.virtual_memory().percent
 
 	# do some printing
-	print(dt, cpu_percent, mean_cpu, "%", cpu_temp, "Celsius")
+	print(dt, cpu_percent, mean_cpu, "%", ram, "% RAM", cpu_temp, "Celsius")
 
 	# do the appending
 	datetime_list.append(dt)
 	cpu_list.append(cpu_percent)
 	mean_cpu_list.append(mean_cpu)
 	cpu_temp_list.append(cpu_temp)
+	ram_list.append(ram)
 
 	# sleep 10 seconds
 	time.sleep(10)
