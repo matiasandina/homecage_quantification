75ba878da48d1f72e907b1aeb40df61674e102a6
matiasandina <matiasandina@gmail.com>
2020-12-07 09:44:48 -0500

    modifying clean cameras

--- clean_cameras.py
+++ clean_cameras.py
@@ -6,11 +6,11 @@ import os
 # -----------------------------------------------
 
 # Thermal Cameras ------
-# this will remove every file except main.py
-# it will also try to remove /media/pi/SD_card but give error because its busy
-os.system("find /media/pi ! -name 'main.py' -type f -exec rm -f {} +")
+# This will remove things that start with 20 (as in 2020 and the next 80 years) 
+# it will not clean EVERYTHING, but close enough
+os.system("/media/pi/*/20* -exec rm -f {} +")
 # now the directories 
-os.system("find /media/pi ! -name 'main.py' -type d -exec rm -f {} +")
+os.system("/media/pi/*/20* -exec rm -f {} +")
 
 # Color Cameras -----
 os.system("find /home/pi/homecage_quantification/ -name '*_opt_flow.csv' -type f -exec rm -f {} +")
